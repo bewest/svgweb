e20346196a66d703cb6274ecaa29501749d097ea
grick23 <grick23@4490c68e-677f-11de-b8ef-473237edd27e>
2009-08-19 03:14:56 +0000

    Issue 221; Fix regression in original fix, for when no entities are present.

--- src/svg.js
+++ src/svg.js
@@ -1121,15 +1121,17 @@ extend(SVGWeb, {
         
     // expand ENTITY definitions
     var entities = svg.match(/.*<!ENTITY\s+(\S+)\s+"([^"]*)"\s*>/mg);
-    var entityName = [];
-    var entityContent = [];
-    for (var i=0; i < entities.length; i++) {
-        var parts = entities[i].match(/.*<!ENTITY\s+(\S+)\s+"([^"]*)"\s*>/m);
-        entityName[i] = parts[1];
-        entityContent[i] = parts[2];
-    }
-    for (i=0; i < entityName.length; i++) {
-        svg = svg.split("&" + entityName[i] + ";").join(entityContent[i]);
+    if (entities) {
+        var entityName = [];
+        var entityContent = [];
+        for (var i=0; i < entities.length; i++) {
+            var parts = entities[i].match(/.*<!ENTITY\s+(\S+)\s+"([^"]*)"\s*>/m);
+            entityName[i] = parts[1];
+            entityContent[i] = parts[2];
+        }
+        for (i=0; i < entityName.length; i++) {
+            svg = svg.split("&" + entityName[i] + ";").join(entityContent[i]);
+        }
     }
        
     if (addMissing) {
