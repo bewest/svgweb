148beaa63d63dd2793fa9c6ec95ef4ec03d5ea8e
grick23 <grick23@4490c68e-677f-11de-b8ef-473237edd27e>
2010-12-24 21:06:22 +0000

    Issue 573; Update examples and documentation to use the new IE 9 compatible object syntax.

--- tests/scripts/convert.py
+++ tests/scripts/convert.py
@@ -6,8 +6,9 @@ import re
 import sys
 
 for filename in glob.glob("../htmlObjectHarness/*.html"):
-    if filename == "../htmlObjectHarness/full-color-prof-01-f.html":
-        continue
+    # used to test the first file
+    #if filename != "../htmlObjectHarness/full-color-prof-01-f.html":
+    #    continue
     if filename == "../htmlObjectHarness/tiny-index.html":
         continue
     if filename == "../htmlObjectHarness/basic-index.html":
@@ -25,8 +26,18 @@ for filename in glob.glob("../htmlObjectHarness/*.html"):
     svgfilename = re.sub("../htmlObjectHarness/basic-", "", svgfilename)
     svgfilename = '../svggen/' + svgfilename
 
-    #content = re.sub('src="\.\.\/\.\.\/html\/svg\.js"', 'src="../../src/svg.js"', content)
-    #content = re.sub("<object.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n", '<!--[if IE]>\n<object src="' + svgfilename + '" classid="image/svg+xml"\n\twidth="480" height="360">\n<![endif]-->\n<!--[if !IE]>-->\n<object data="' + svgfilename + '" type="image/svg+xml"\n\twidth="480" height="360">\n<!--<![endif]-->\n</object>\n', content)
+    content = re.sub(r'<!--\[if IE\]>\n<object src="([^"]*)" classid="image/svg\+xml"\n.*\n.*\n.*\n.*\n.*\n.*\n.*\n', 
+                   r'\t\t\t\t\t\t<!--[if !IE]>-->\n' + 
+                    '\t\t\t\t\t\t  <object data="\\1" type="image/svg+xml"\n' + 
+                    '\t\t\t\t\t\t          width="480" height="360"> <!--<![endif]-->\n' + 
+                    '\t\t\t\t\t\t<!--[if lt IE 9]>\n' +
+                    '\t\t\t\t\t\t  <object src="\\1" classid="image/svg+xml"\n' +
+                    '\t\t\t\t\t\t          width="480" height="360"> <![endif]-->\n' + 
+                    '\t\t\t\t\t\t<!--[if gte IE 9]>\n' +
+                    '\t\t\t\t\t\t  <object data="\\1" type="image/svg+xml"\n' + 
+                    '\t\t\t\t\t\t          width="480" height="360"> <![endif]-->\n' + 
+                    '\t\t\t\t\t\t  </object>\n\n',
+                    content)
 
     outfile.write(content)
     outfile.close()
